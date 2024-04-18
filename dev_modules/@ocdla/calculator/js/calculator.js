0e5d253342d04291940bc26fd4ebf99c26a979b7
KelsieMcDonald-2003 <kelsie.lin.mcdonald@gmail.com>
2024-04-18 09:29:11 -0700

    Added addObserver and notifyObservers methods. addObserver adds an observer to the list of observers. notifyObservers method notifies all registered observers about some event.

--- dev_modules/@ocdla/calculator/js/calculator.js
+++ dev_modules/@ocdla/calculator/js/calculator.js
@@ -4,10 +4,19 @@ export class Calculator {
     static ALLOWED_KEYS = ['1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '*', '(', '0', ')', '.', '/', '%', '^', '='];
 
     constructor() {
-        
+        this.observers = [];
+    }
+
+    addObserver(observer) {
+        this.observers.push(observer);
+    }
+
+    notifyObservers(event, data) {
+        this.observers.forEach(observer => observer.update(event, data));
     }
 
     solve(x) {
+        this.notifyObservers('calculation', {userinput: x, solution: result});
         return math.evaluate(x);
     }
 
